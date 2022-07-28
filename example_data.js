070e34f92b0073849337bd5308d1a12d14eeb53c
DarkInk <darkerink@zenaxis.org>
2022-07-28 04:28:37 -0500

    Delete example_data.js

--- example_data.js
+++
@@ -1,7 +0,0 @@
-/*
-This is just some Example Data for Guilds, Channels, Roles, Guild Members, Messages, Users (Private, Public), Deleted Users, Deleted Users Messages
-Its just for Examples and to be used to Base the API around. It also explains some stuff
-
-Remember This is just EXAMPLE Info, I want to base most stuff around this but not everything will be exact
-Some things will be changed/added and or removed.
-*/
