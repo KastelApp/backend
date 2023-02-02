51f1d1dad97c3f92a4aff754172d6eabf4494b5c
DarkInk <darkerink@zenaxis.org>
2023-02-02 14:02:15 -0600

    Base64
    Base64 Functions

---
+++ src/Utils/Classes/Base64.ts
@@ -0,0 +1,24 @@
+class Base64 {
+  Encode(string: string): string {
+    // Convert the string to base64
+    const base64 = Buffer.from(string).toString("base64");
+
+    // Replace + with F, / with q, and = with zT
+    return base64.replace(/\+/g, "F").replace(/\//g, "q").replace(/=+$/, "zT");
+  }
+
+  Decode(string: string): string {
+    // Replace F with +, q with /, and zT with =
+    const base64 = string
+      .replace(/F/g, "+")
+      .replace(/q/g, "/")
+      .replace(/zT$/, "");
+
+    // Convert the base64 to string
+    return Buffer.from(base64, "base64").toString("utf8");
+  }
+}
+
+export default Base64;
+
+export { Base64 };
