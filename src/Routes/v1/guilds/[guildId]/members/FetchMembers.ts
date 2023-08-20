4a634b8dad61643d2b5dd5c806f36b185892847d
Darker-Ink <rstonehouse66@gmail.com>
2023-08-20 04:23:08 -0500

    feat: placeholder routes

---
+++ src/Routes/v1/guilds/[guildId]/members/FetchMembers.ts
@@ -0,0 +1,32 @@
+/* !
+ *   ██╗  ██╗ █████╗ ███████╗████████╗███████╗██╗
+ *   ██║ ██╔╝██╔══██╗██╔════╝╚══██╔══╝██╔════╝██║
+ *  █████╔╝ ███████║███████╗   ██║   █████╗  ██║
+ *  ██╔═██╗ ██╔══██║╚════██║   ██║   ██╔══╝  ██║
+ * ██║  ██╗██║  ██║███████║   ██║   ███████╗███████╗
+ * ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝╚══════╝
+ * Copyright(c) 2022-2023 DarkerInk
+ * GPL 3.0 Licensed
+ */
+
+import type { Request, Response } from 'express';
+import type App from '../../../../../Utils/Classes/App';
+import Route from '../../../../../Utils/Classes/Route.js';
+
+export default class Main extends Route {
+	public constructor(App: App) {
+		super(App);
+
+		this.Methods = ['GET'];
+
+		this.Middleware = [];
+
+		this.AllowedContentTypes = [];
+
+		this.Routes = ['/'];
+	}
+
+	public override Request(_: Request, Res: Response): void {
+		Res.send('ok')
+	}
+}
