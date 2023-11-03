8aba760dae16c3b19537094eaaf70fca7f9076f7
Jdog <jdog.cool03@gmail.com>
2023-11-03 15:09:27 -0400

    feat: user settings

---
+++ src/Routes/v1/users/@me/UserSettings.ts
@@ -0,0 +1,107 @@
+/* !
+ *   ██╗  ██╗ █████╗ ███████╗████████╗███████╗██╗
+ *   ██║ ██╔╝██╔══██╗██╔════╝╚══██╔══╝██╔════╝██║
+ *  █████╔╝ ███████║███████╗   ██║   █████╗  ██║
+ *  ██╔═██╗ ██╔══██║╚════██║   ██║   ██╔══╝  ██║
+ * ██║  ██╗██║  ██║███████║   ██║   ███████╗███████╗
+ * ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝   ╚═╝   ╚══════╝╚══════╝
+ * Copyright(c) 2022-2023 DarkerInk
+ * GPL 3.0 Licensed
+ */
+
+import type App from '../../../../Utils/Classes/App.ts';
+import { Encryption } from '../../../../Utils/Classes/Encryption.ts';
+import type { Request, Response } from 'express';
+import Route from '../../../../Utils/Classes/Route.ts';
+import Settings from '../../../../Utils/Cql/Types/Settings.ts';
+import User from '../../../../Middleware/User.ts';
+
+export default class UserSettings extends Route {
+	public constructor(App: App) {
+		super(App);
+
+		this.Methods = ['GET'];
+
+		this.Middleware = [
+			User({
+				AccessType: 'LoggedIn',
+				AllowedRequesters: 'User',
+				App,
+			}),
+		];
+
+		this.AllowedContentTypes = [];
+
+		this.Routes = ['/settings'];
+	}
+
+	public override async Request(Req: Request<{ userId: string }>, Res: Response) {
+		switch(Req.methodi){
+			case 'GET': {
+				await this.FetchSettings(Req, Res);
+				break;
+			}
+
+			case 'PATCH': {
+				if(Req.path.endsWith('/fetch')){
+					Req.fourohfourit();
+					break;
+				}
+
+				await this.PatchSettings(Req, Res);
+				break;
+			}
+
+			default: {
+				Req.fourohfourit();
+				break;
+			}
+		}
+	}
+
+	public async FetchSettings(Req: Request<{ userId: string }>, Res: Response){
+		const UserSettings = await this.App.Cassandra.Models.Settings.get({
+			UserId: Encryption.Encrypt(Req.user.Id)
+		});
+
+		if(!UserSettings) return null;
+
+		return Res.send(Encryption.CompleteDecryption(UserSettings));
+	}
+
+	public async PatchSettings(Req: Request<{ userId: string }, any, Settings>, Res: Response){
+		const { Bio, Language, Mentions, Presence, Privacy, Status, Theme, Tokens } = Req.body;
+
+		const SettingsObject: Settings = {
+			Bio: Encryption.Encrypt(Bio),
+			Language: Encryption.Encrypt(Language),
+			MaxFileUploadSize: this.App.Constants.Settings.Max.MaxFileSize,
+			MaxGuilds: this.App.Constants.Settings.Max.GuildCount,
+			Mentions: Mentions,
+			Presence: Presence ?? this.App.Constants.Presence.Online,
+			Privacy: Privacy,
+			Status: Encryption.Encrypt(Status),
+			Theme: Encryption.Encrypt(Theme),
+			Tokens: Tokens,
+			UserId: Encryption.Encrypt(Req.user.Id)
+		}
+
+		await Promise.all([this.App.Cassandra.Models.Settings.update({
+			Bio: Encryption.Encrypt(Bio),
+			Language: Encryption.Encrypt(Language),
+			MaxFileUploadSize: this.App.Constants.Settings.Max.MaxFileSize,
+			MaxGuilds: this.App.Constants.Settings.Max.GuildCount,
+			Mentions: Mentions,
+			Presence: Presence ?? this.App.Constants.Presence.Online,
+			Privacy: Privacy,
+			Status: Encryption.Encrypt(Status),
+			Theme: Encryption.Encrypt(Theme),
+			Tokens: Tokens,
+			UserId: Encryption.Encrypt(Req.user.Id)
+		})])
+
+		return Res.status(201).json({
+			...Encryption.CompleteDecryption(SettingsObject)
+		});
+	}
+}
