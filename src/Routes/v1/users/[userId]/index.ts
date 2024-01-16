0a435d73a24e763b1d5eaf4c95b2309178d9cccb
Darker-Ink <rstonehouse66@gmail.com>
2024-01-16 00:04:49 -0600

    feat: Fetching user info & profile info

--- src/Routes/v1/users/[userId]/index.ts
+++ src/Routes/v1/users/[userId]/index.ts
@@ -1,8 +1,26 @@
+import type { UserMiddlewareType } from "@/Middleware/User.ts";
+import userMiddleware from "@/Middleware/User.ts";
 import type App from "@/Utils/Classes/App.ts";
+import FlagFields from "@/Utils/Classes/BitFields/Flags.ts";
+import Encryption from "@/Utils/Classes/Encryption.ts";
+import errorGen from "@/Utils/Classes/ErrorGen.ts";
 import ContentTypes from "@/Utils/Classes/Routing/Decorators/ContentTypes.ts";
 import Description from "@/Utils/Classes/Routing/Decorators/Description.ts";
 import Method from "@/Utils/Classes/Routing/Decorators/Method.ts";
+import Middleware from "@/Utils/Classes/Routing/Decorators/Middleware.ts";
+import type { CreateRoute } from "@/Utils/Classes/Routing/Route.ts";
 import Route from "@/Utils/Classes/Routing/Route.ts";
+
+interface User {
+	avatar: string | null;
+	bio?: string | null;
+	flags: string;
+	globalNickname: string | null;
+	id: string;
+	publicFlags: string;
+	tag: string;
+	username: string;
+}
 
 export default class Fetch extends Route {
 	public constructor(App: App) {
@@ -10,9 +28,60 @@ export default class Fetch extends Route {
 	}
 
 	@Method("get")
-	@Description("Change this Description when working on this route")
-	@ContentTypes("application/json")
-	public get() {
-		return {};
+	@Description("Fetch a user by their ID")
+	@ContentTypes("any")
+	@Middleware(userMiddleware({
+		AccessType: "LoggedIn",
+		AllowedRequesters: ["User"]
+	}))
+	public async getProfile({
+		params,
+		query,
+		set
+	}: CreateRoute<"/users/:userId", any, [UserMiddlewareType]>) {
+		const fetchedUser = await this.App.Cassandra.Models.User.get({
+			userId: Encryption.encrypt(params.userId)
+		});
+
+		if (!fetchedUser) {
+			const userNotFound = errorGen.InvalidUser();
+
+			userNotFound.addError({
+				user: {
+					code: "InvalidUser",
+					message: "The requested user does not exist, or they have blocked you."
+				}
+			})
+
+			set.status = 404;
+			
+			return userNotFound.toJSON()
+		}
+
+		const flags = new FlagFields(fetchedUser.flags, fetchedUser?.publicFlags ?? 0);
+
+		const include = query.include?.split(",") ?? [];
+
+		const userObject: User = {
+			id: fetchedUser.userId,
+			username: fetchedUser.username,
+			globalNickname: fetchedUser.globalNickname,
+			tag: fetchedUser.tag,
+			avatar: fetchedUser.avatar,
+			publicFlags: String(flags.PublicFlags.cleaned),
+			flags: String(flags.PrivateFlags.cleaned),
+		};
+
+		if (include.includes("bio")) {
+			const bio = await this.App.Cassandra.Models.Settings.get({
+				userId: Encryption.encrypt(params.userId)
+			}, {
+				fields: ["bio"]
+			});
+
+			userObject.bio = bio?.bio ?? null;
+		}
+
+		return Encryption.completeDecryption(userObject);
 	}
 }
--- src/Routes/v1/users/[userId]/profile.ts
+++ src/Routes/v1/users/[userId]/profile.ts
@@ -1,8 +1,24 @@
+import type { UserMiddlewareType } from "@/Middleware/User.ts";
+import userMiddleware from "@/Middleware/User.ts";
 import type App from "@/Utils/Classes/App.ts";
+import Encryption from "@/Utils/Classes/Encryption.ts";
+import errorGen from "@/Utils/Classes/ErrorGen.ts";
 import ContentTypes from "@/Utils/Classes/Routing/Decorators/ContentTypes.ts";
 import Description from "@/Utils/Classes/Routing/Decorators/Description.ts";
 import Method from "@/Utils/Classes/Routing/Decorators/Method.ts";
+import Middleware from "@/Utils/Classes/Routing/Decorators/Middleware.ts";
+import type { CreateRoute } from "@/Utils/Classes/Routing/Route.ts";
 import Route from "@/Utils/Classes/Routing/Route.ts";
+
+// ? Why would we want to have a "profile" route instead of adding it to the normal user route
+// ? Great question, the reason is I want it to be the same as the @me route, since the profile meta data is sent in the 
+// ? Gateway payload for identify, I want to keep it the same as the @me route, so that way I can easily document it
+
+interface ProfileResponse {
+	connections: unknown[] // TODO: Add connections (Discord, Twitter (X), Github, Steam, Spotify (Not sure if we can do this one), Reddit, Youtube, Twitch)
+	mutualFriends: string[];
+	mutualGuilds: string[];
+}
 
 export default class Profile extends Route {
 	public constructor(App: App) {
@@ -10,9 +26,45 @@ export default class Profile extends Route {
 	}
 
 	@Method("get")
-	@Description("Change this Description when working on this route")
-	@ContentTypes("application/json")
-	public getProfile() {
-		return {};
+	@Description("Fetch a users profile")
+	@ContentTypes("any")
+	@Middleware(userMiddleware({
+		AccessType: "LoggedIn",
+		AllowedRequesters: ["User"]
+	}))
+	public async getProfile({
+		params,
+		set,
+		user
+	}: CreateRoute<"/users/:userId/profile", any, [UserMiddlewareType]>) {
+		
+		const fetchedUser = await this.App.Cassandra.Models.User.get({
+			userId: Encryption.encrypt(params.userId)
+		});
+
+		if (!fetchedUser) {
+			const userNotFound = errorGen.InvalidUser();
+
+			userNotFound.addError({
+				user: {
+					code: "InvalidUser",
+					message: "The requested user does not exist, or they have blocked you."
+				}
+			})
+
+			set.status = 404;
+			
+			return userNotFound.toJSON()
+		}
+		
+		const mutualGuilds = user.guilds.filter((guild) => fetchedUser.guilds.includes(Encryption.encrypt(guild)));
+	
+		const response: ProfileResponse = {
+			connections: [],
+			mutualFriends: [],
+			mutualGuilds
+		}
+		
+		return response;
 	}
 }
